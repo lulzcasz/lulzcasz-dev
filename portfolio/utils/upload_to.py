0d0487751357203f522f846395fd6c2ee4a77a4b
Lucas Reis <reisdossousalucasedson@gmail.com>
2026-05-26 23:48:16 -0300

    refactor(portfolio): simplify profile avatar upload path by removing date

--- portfolio/utils/upload_to.py
+++ portfolio/utils/upload_to.py
@@ -4,4 +4,4 @@ from django.utils.timezone import now
 
 
 def profile_avatar_path(instance, filename):
-    return f'images/avatars/{now().strftime("%Y/%m/%d")}/{uuid4()}/raw{splitext(filename)[1]}'
+    return f'images/avatars/{uuid4()}/raw{splitext(filename)[1]}'
