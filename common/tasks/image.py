8f3ca8895ced719d74e60c0617bb832d1fac3522
Lucas Reis <reisdossousalucasedson@gmail.com>
2026-06-08 23:40:31 -0300

    perf(common): prioritize webp and optimize FFmpeg arguments for VPS

--- common/tasks/image.py
+++ common/tasks/image.py
@@ -1,6 +1,5 @@
 import os
 from celery import shared_task
-from PIL import Image
 from common.utils.image import download_to_temp, process_and_save_image
 
 @shared_task(bind=True)
@@ -10,12 +9,12 @@ def process_image(self, relative_path, kind):
     with download_to_temp(relative_path) as input_path:
         if kind == 'cover':
             versions = [
-                {'size': 'large',  'ext': 'avif', 'w': 1200, 'h': 630, 'args': ['-threads', '1', '-c:v', 'libaom-av1', '-still-picture', '1', '-crf', '24']},
-                {'size': 'large',  'ext': 'webp', 'w': 1200, 'h': 630, 'args': ['-threads', '1', '-c:v', 'libwebp', '-q:v', '80']},
-                {'size': 'medium', 'ext': 'avif', 'w': 960,  'h': 504, 'args': ['-threads', '1', '-c:v', 'libaom-av1', '-still-picture', '1', '-crf', '16']},
-                {'size': 'medium', 'ext': 'webp', 'w': 960,  'h': 504, 'args': ['-threads', '1', '-c:v', 'libwebp', '-q:v', '85']},
-                {'size': 'small',  'ext': 'avif', 'w': 480,  'h': 252, 'args': ['-threads', '1', '-c:v', 'libaom-av1', '-still-picture', '1', '-crf', '8']},
-                {'size': 'small',  'ext': 'webp', 'w': 480,  'h': 252, 'args': ['-threads', '1', '-c:v', 'libwebp', '-q:v', '92']},
+                {'size': 'large',  'ext': 'webp', 'w': 1200, 'h': 630, 'args': ['-threads', '2', '-c:v', 'libwebp', '-q:v', '80']},
+                {'size': 'medium', 'ext': 'webp', 'w': 960,  'h': 504, 'args': ['-threads', '2', '-c:v', 'libwebp', '-q:v', '85']},
+                {'size': 'small',  'ext': 'webp', 'w': 480,  'h': 252, 'args': ['-threads', '2', '-c:v', 'libwebp', '-q:v', '92']},
+                {'size': 'large',  'ext': 'avif', 'w': 1200, 'h': 630, 'args': ['-threads', '2', '-c:v', 'libaom-av1', '-still-picture', '1', '-cpu-used', '6', '-crf', '24']},
+                {'size': 'medium', 'ext': 'avif', 'w': 960,  'h': 504, 'args': ['-threads', '2', '-c:v', 'libaom-av1', '-still-picture', '1', '-cpu-used', '6', '-crf', '16']},
+                {'size': 'small',  'ext': 'avif', 'w': 480,  'h': 252, 'args': ['-threads', '2', '-c:v', 'libaom-av1', '-still-picture', '1', '-cpu-used', '6', '-crf', '8']},
             ]
             
             for config in versions:
@@ -28,38 +27,24 @@ def process_image(self, relative_path, kind):
                 process_and_save_image(input_path, final_path, args)
 
         elif kind == 'content_image':
-            with Image.open(input_path) as img:
-                is_animated = getattr(img, 'is_animated', False)
+            vf_scale_crop = "scale='min(960,iw)':'min(620,ih)':force_original_aspect_ratio=decrease,crop=trunc(iw/2)*2:trunc(ih/2)*2"
 
-            vf_scale_crop = "fps=15,scale='min(960,iw)':'min(620,ih)':force_original_aspect_ratio=decrease,crop=trunc(iw/2)*2:trunc(ih/2)*2"
-
-            content_versions = ['avif', 'webp']
+            content_versions = ['webp', 'avif']
 
             for ext in content_versions:
                 final_path = os.path.join(directory, f'processed.{ext}')
 
-                if is_animated:
-                    if ext == 'avif':
-                        args = [
-                            '-threads', '1', '-c:v', 'libsvtav1', '-crf', '38', '-preset', '8',
-                            '-vf', vf_scale_crop, '-pix_fmt', 'yuv420p'
-                        ]
-                    else:
-                        args = [
-                            '-threads', '1', '-c:v', 'libwebp', '-loop', '0', '-q:v', '70',
-                            '-vf', vf_scale_crop, '-pix_fmt', 'yuv420p'
-                        ]
-                else:
-                    if ext == 'avif':
-                        args = [
-                            '-threads', '1', '-c:v', 'libaom-av1', '-still-picture', '1', '-crf', '16', '-cpu-used', '4',
-                            '-vf', vf_scale_crop, '-pix_fmt', 'yuv420p'
-                        ]
-                    else:
-                        args = [
-                            '-threads', '1', '-c:v', 'libwebp', '-q:v', '85',
-                            '-vf', vf_scale_crop, '-pix_fmt', 'yuv420p'
-                        ]
+                if ext == 'avif':
+                    args = [
+                        '-threads', '2', '-c:v', 'libaom-av1', '-still-picture', '1', 
+                        '-crf', '16', '-cpu-used', '6',
+                        '-vf', vf_scale_crop, '-pix_fmt', 'yuv420p'
+                    ]
+                else: # webp
+                    args = [
+                        '-threads', '2', '-c:v', 'libwebp', '-q:v', '85',
+                        '-vf', vf_scale_crop, '-pix_fmt', 'yuv420p'
+                    ]
                 
                 process_and_save_image(input_path, final_path, args)
 
