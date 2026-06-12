dd7ea34a23f09419c75e274d3bf7c73285fed384
Lucas Reis <reisdossousalucasedson@gmail.com>
2026-06-12 17:18:35 -0300

    feat(blog): finalize product card layout with description and dynamic colors

--- blog/admin.py
+++ blog/admin.py
@@ -1,21 +1,5 @@
 from django.contrib import admin
 from blog.models import Genre, Category, Post
-from products.models import Product
-
-
-class ProductInline(admin.TabularInline):
-    model = Post.products.through
-    extra = 1
-
-    fields = ('product_id_display', 'product')
-
-    readonly_fields = ('product_id_display',)
-
-    @admin.display(description="product id")
-    def product_id_display(self, instance):
-        if instance.pk and instance.product:
-            return f"ID: {instance.product.id}"
-        return "-"
 
 
 @admin.register(Genre)
@@ -31,10 +15,6 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid', 'created_at', 'updated_at')
-
-    inlines = [ProductInline]
-
-    exclude = ('products',)
 
     def get_exclude(self, request, obj=None):
         if not obj:
