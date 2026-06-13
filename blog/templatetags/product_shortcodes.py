2235c6bf3c73c3568bcb4ef0b2a2faa2e5218327
Lucas Reis <reisdossousalucasedson@gmail.com>
2026-06-13 20:03:59 -0300

    feat(blog): implement blog post shortcodes and card component

--- blog/templatetags/product_shortcodes.py
+++ blog/templatetags/product_shortcodes.py
@@ -2,6 +2,7 @@ import re
 from django import template
 from django.template.loader import render_to_string
 from products.models import Product
+from blog.models import Post
 
 register = template.Library()
 
@@ -23,3 +24,26 @@ def render_product_shortcodes(content):
             return f""
 
     return re.sub(SHORTCODE_REGEX, replace_with_card, content)
+
+WRAPPED_POST_REGEX = re.compile(r'<p[^>]*>\s*\[post-(\d+)\]\s*</p>')
+
+INLINE_POST_REGEX = re.compile(r'\[post-(\d+)\]')
+
+@register.filter(name='render_post_shortcodes')
+def render_post_shortcodes(content):
+    if not content:
+        return ""
+
+    def replace_with_card(match):
+        post_id = match.group(1)
+        try:
+            post = Post.objects.get(id=post_id)
+            return render_to_string('blog/post.html', {'post': post})
+        except Post.DoesNotExist:
+            return ""
+
+    content = re.sub(WRAPPED_POST_REGEX, replace_with_card, content)
+
+    content = re.sub(INLINE_POST_REGEX, replace_with_card, content)
+
+    return content
