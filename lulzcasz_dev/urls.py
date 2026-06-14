b63953cdc3db76c60834420d897e63739d6c1730
Lucas Reis <reisdossousalucasedson@gmail.com>
2026-06-14 16:09:55 -0300

    feat: move blog to root and portfolio to portfolio path

--- lulzcasz_dev/urls.py
+++ lulzcasz_dev/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
@@ -13,9 +12,8 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
-    path('blog/', include('blog.urls')),
+    path('portfolio/', include('portfolio.urls')),
     path('', include('common.urls')),
-    path('', include('portfolio.urls')),
     path(
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
@@ -26,4 +24,5 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
+    path('', include('blog.urls')),
 ]
